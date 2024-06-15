4a19015314f6b15df8f2331a57a940cd513e0155
Imani Niyigena <ImaniN@hollywoodbets.net>
2024-06-15 11:40:35 +0200

    Branch cleaning

--- Source Code/C#/AIPlayer.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public class AIPlayer : Player {
-	public override void makeMove (GameData game)
-	{
-		throw new System.NotImplementedException ();
-	}
-}
--- Source Code/C#/UIPlayer.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public class UIPlayer : Player {
-	public override void makeMove (GameData game)
-	{
-		throw new System.NotImplementedException ();
-	}
-}
