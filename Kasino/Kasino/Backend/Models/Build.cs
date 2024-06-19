b6aabda31e9409ca62e1e88bf4cdf48028da19df
Imani Niyigena <ImaniN@hollywoodbets.net>
2024-06-19 11:15:43 +0200

    Refactor Build class and update constructors
    - Moved Build class to Kasino.Backend.Models namespace.
    - Made Owner property of Build class nullable.
    - Removed one constructor and added two new constructors for flexibility in Build instantiation.
    - Minor modification to Cards property for alignment with class restructuring.

--- Kasino/Kasino/Backend/Models/Build.cs
+++ Kasino/Kasino/Backend/Models/Build.cs
@@ -1,29 +1,36 @@
 using Kasino.Models;
 
-
-/// <summary>
-/// Represents a build in the game, consisting of a collection of cards and the player who owns the build.
-/// </summary>
-public class Build
+namespace Kasino.Backend.Models
 {
   /// <summary>
-  /// Gets or sets the owner of the build.
+  /// Represents a build in the game, consisting of a collection of cards and the player who owns the build.
   /// </summary>
-  public Player Owner { get; set; }
-
-  /// <summary>
-  /// Gets or sets the list of cards that make up the build.
-  /// </summary>
-  public List<Card> Cards { get; set; } = new List<Card>();
-
-  /// <summary>
-  /// Initializes a new instance of the <see cref="Build"/> class with a specified owner and list of cards.
-  /// </summary>
-  /// <param name="owner">The player who owns the build.</param>
-  /// <param name="cards">The list of cards that make up the build.</param>
-  public Build(Player owner, List<Card> cards)
+  public class Build
   {
-    Owner = owner;
-    Cards = cards;
+    /// <summary>
+    /// Gets or sets the owner of the build.
+    /// </summary>
+    public Player? Owner { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of cards that make up the build.
+    /// </summary>
+    public List<Card> Cards { get; set; }
+
+    public Build(List<Card> cards)
+    {
+      Cards = cards;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Build"/> class with a specified owner and list of cards.
+    /// </summary>
+    /// <param name="owner">The player who owns the build.</param>
+    /// <param name="cards">The list of cards that make up the build.</param>
+    public Build(Player owner, List<Card> cards)
+    {
+      Owner = owner;
+      Cards = cards;
+    }
   }
 }
